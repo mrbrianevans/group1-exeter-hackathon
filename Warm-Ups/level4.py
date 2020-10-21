8f7d19c2cbdbd9f32b48ba5fb2b11b5b417efdf3
armorbug <73236178+armorbug@users.noreply.github.com>
2020-10-21 16:22:39 +0100

    Complete

--- Warm-Ups/level4.py
+++ Warm-Ups/level4.py
@@ -4,7 +4,15 @@ def double_swap(string, a, b):
     # Add your solution here. You can use additional functions if need be.
     # Don't forget to add a DocString for all your functions and comment your code.
     # Your functions should return values rather than printing the result although you can use printing for testing purposes.
-    return "Your Return Value"
+    new_string=""
+    for character in string:
+        if character==a:
+            new_string=new_string+b
+        elif character==b:
+            new_string=new_string+a
+        else:
+            new_string=new_string+character
+    return new_string #"Your Return Value"
 
 if __name__ == '__main__':
     # Add any code to test your solution here
