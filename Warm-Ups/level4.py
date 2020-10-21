60fe10c3b59a9e65149702fde257a9a0c222c76c
Christopher Man <christophergayiuman@gmail.com>
2020-10-22 02:00:16 +0800

    TurtleDraw

--- Warm-Ups/level4.py
+++ Warm-Ups/level4.py
@@ -6,7 +6,6 @@ def double_swap(string, a, b):
     # Your functions should return values rather than printing the result although you can use printing for testing purposes.
 
     """
-
     :param string: creates an empty character
     :param a: the characters vendi
     :param b: vidi
@@ -27,4 +26,3 @@ if __name__ == '__main__':
     # Add any code to test your solution here
     # As per the example, this should return ienv, ivdv, ivcv
     print(double_swap('veni, vidi, vici', 'v', 'i'))
-    
